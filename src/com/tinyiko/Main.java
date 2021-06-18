26a0a491488c11abd7a0868fdb934c1cb8471d19
f5295785 <tinyiko.chauke1@fnb.co.za>
2021-06-18 15:46:05 +0200

    variables example

--- src/com/tinyiko/Main.java
+++ src/com/tinyiko/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world this is my first java 101 program");
+        //variables
+        byte byteVariable = -128;
     }
 }
