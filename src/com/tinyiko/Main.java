f86f727ad5d8184b30a747e187fe443ea6574280
f5295785 <tinyiko.chauke1@fnb.co.za>
2021-06-18 18:25:21 +0200

    added arithmetic operations examples

--- src/com/tinyiko/Main.java
+++ src/com/tinyiko/Main.java
@@ -4,5 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world this is my first java 101 program");
+
+        //NOTE BODMAS formula is always applied under the hood
+        System.out.println(10 + 2 + 90);
+        System.out.println(10 - 2);
+
+        //one can also use parenthesis to emphasize bodmas
+        System.out.println(10 * (2 + 7) - 10);
+        System.out.println(10 % 3);
     }
 }
