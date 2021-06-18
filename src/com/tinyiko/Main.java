d288b7cc77260454a8bdb58bc6b7894078b7bd87
f5295785 <tinyiko.chauke1@fnb.co.za>
2021-06-18 16:15:59 +0200

    adding underscore for easy readability

--- src/com/tinyiko/Main.java
+++ src/com/tinyiko/Main.java
@@ -7,8 +7,8 @@ public class Main {
         //variables PRIMITIVE DATA TPYES
         byte byteVariable = -128; //-128 to 127 size 1 byte
         short shortVariable = 32767; //-32768 to 32767 size 2 bytes
-        int intVariable = 2147483647; //-2147483648 to 2147483647 size is 4 bytes
-        long longVariable = 922337203654775808L; // -922337203654775808 to 922337203654775808 size is 8 bytes
+        int intVariable = 2_147_483_647; //-2147483648 to 2147483647 size is 4 bytes
+        long longVariable = 922_337_203_654_775_808L; // -922337203654775808 to 922337203654775808 size is 8 bytes
         float floatVariable= 1; // size is 4 bytes
         double doubleVariable = 10.00; //size is 8 bytes
         boolean booleanVariable = true; //true or false size is 1 bit
