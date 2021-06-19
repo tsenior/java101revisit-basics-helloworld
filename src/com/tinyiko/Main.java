0cb01c76a928c17c28243ecf43f520133c8eed5b
f5295785 <tinyiko.chauke1@fnb.co.za>
2021-06-19 04:00:40 +0200

    added Math class example

--- src/com/tinyiko/Main.java
+++ src/com/tinyiko/Main.java
@@ -4,5 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world this is my first java 101 program");
+
+        //MATH CLASS
+
+        System.out.println(Math.abs(-10));
+        System.out.println(Math.max(10, 99));
+        System.out.println(Math.min(99, 10));
+        System.out.println(Math.pow(5, 2));
+        System.out.println(Math.sqrt(25));
+        System.out.println(Math.PI);
     }
 }
