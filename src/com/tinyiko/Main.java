949f025c3f4f97c3524dfe5605bb0b7cf288b099
f5295785 <tinyiko.chauke1@fnb.co.za>
2021-06-19 04:09:56 +0200

    added comparison operators example

--- src/com/tinyiko/Main.java
+++ src/com/tinyiko/Main.java
@@ -4,5 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world this is my first java 101 program");
+
+        //COMPARISON OPERATOR
+
+        int tinyikoAge = 20;
+        int seniorAge = 20;
+
+        System.out.println(tinyikoAge > seniorAge);
+        System.out.println(tinyikoAge >= seniorAge);
+        System.out.println(tinyikoAge < seniorAge);
+        System.out.println(tinyikoAge <= seniorAge);
+        System.out.println(tinyikoAge == seniorAge);
+        System.out.println(tinyikoAge != seniorAge);
+
     }
 }
