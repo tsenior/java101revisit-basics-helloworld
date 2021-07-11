1c3057c1ec8bd65d4914ea6f8334306ab106c54a
f5295785 <tinyiko.chauke1@fnb.co.za>
2021-07-11 11:57:04 +0200

    Arrays

--- src/com/tinyiko/Main.java
+++ src/com/tinyiko/Main.java
@@ -1,8 +1,20 @@
 package com.tinyiko;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world this is my first java 101 program");
+        System.out.println("ARRAYS");
+
+        System.out.println("Numbers");
+        int [] numbers = {10, 2, 100};
+        System.out.println(Arrays.toString(numbers));
+
+        System.out.println("String Array");
+
+        String [] stringsArray = {"abc", "def", "ghi", "jkl"};
+        System.out.println(Arrays.toString(stringsArray));
     }
 }
