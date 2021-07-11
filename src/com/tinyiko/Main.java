ae26d11812a174f94f0f9411bbfbc3e639309e77
f5295785 <tinyiko.chauke1@fnb.co.za>
2021-07-11 11:21:10 +0200

    if statements

--- src/com/tinyiko/Main.java
+++ src/com/tinyiko/Main.java
@@ -4,5 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world this is my first java 101 program");
+        System.out.println("IF statements");
+
+        //check if someone is an adult or nor
+        //An adult is anyone over the age of 18
+
+        int age = 15;
+
+        if (age >= 18){
+            System.out.println("you are old enough");
+        }else if (age >= 16 && age < 18){
+            System.out.println("am almost an adult");
+        }else {
+            System.out.println("go home kid");
+        }
     }
 }
