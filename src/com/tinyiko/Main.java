ea7f7970ad27cda60733cbb64bedb9d6435ff040
f5295785 <tinyiko.chauke1@fnb.co.za>
2021-07-12 07:53:11 +0200

    while loop

--- src/com/tinyiko/Main.java
+++ src/com/tinyiko/Main.java
@@ -4,5 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world this is my first java 101 program");
+        System.out.println("WHILE LOOP");
+
+        int count = 0;
+
+        while (count <= 20){
+            System.out.println( count);
+            count++;
+        }
     }
 }
