9dd0bd74f8cd60020b8e84abb74790794edcdc84
f5295785 <tinyiko.chauke1@fnb.co.za>
2021-07-12 07:23:19 +0200

    enhanced loop

--- src/com/tinyiko/Main.java
+++ src/com/tinyiko/Main.java
@@ -4,5 +4,40 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world this is my first java 101 program");
+        // Loops
+
+        int[] numbers = {1, 2, 3, 4, 5, 6 , 10, 20, 30};
+        String[] names = {"tinyiko","senior","chauke"};
+
+        System.out.println("NUMBERS FOR I ARRAY");
+        for (int i = 0; i < numbers.length; i++){
+            System.out.println(numbers[i]);
+        }
+
+        System.out.println("reversed numbers loop");
+        for (int i = numbers.length - 1; i >=0; i--){
+            System.out.println(numbers[i]);
+        }
+
+        System.out.println("NUMBERS ENHANCED FOR LOOP");
+        for (int number : numbers){
+            System.out.println(number);
+        }
+
+
+        System.out.println("NAMES FOR I ARRAY");
+        for (int i = 0; i < names.length; i++){
+            System.out.println(names[i]);
+        }
+
+        System.out.println("reversed names loop");
+        for (int i = names.length -1; i >=0; i--){
+            System.out.println(names[i]);
+        }
+
+        System.out.println("NAMES ENHANCED FOR LOOP");
+        for (String name : names){
+            System.out.println(name);
+        }
     }
 }
