f808dc8eb8b4425f8c755ac63b04cf1aaa782ab8
f5295785 <tinyiko.chauke1@fnb.co.za>
2021-07-12 08:10:18 +0200

    scanner class

--- src/com/tinyiko/Main.java
+++ src/com/tinyiko/Main.java
@@ -1,8 +1,31 @@
 package com.tinyiko;
 
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world this is my first java 101 program");
+        System.out.println("user input scanner");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter name");
+
+        String name = scanner.nextLine();
+        System.out.println("Hello "+ name);
+
+        System.out.println("enter your age");
+        int age = scanner.nextInt();
+        int year = LocalDate.now().minusYears(age).getYear();
+        System.out.println("you were born in "+year);
+
+        if (age < 18){
+            System.out.println("you are old enough");
+        }else {
+            System.out.println("you are a child");
+        }
+
+
     }
 }
