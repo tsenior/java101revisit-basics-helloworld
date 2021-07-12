5424338451a4fb4322541dc0fc349d8d8fb17a89
f5295785 <tinyiko.chauke1@fnb.co.za>
2021-07-12 07:45:18 +0200

    break continue statements

--- src/com/tinyiko/Main.java
+++ src/com/tinyiko/Main.java
@@ -4,5 +4,26 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world this is my first java 101 program");
+        System.out.println("BREAK CONTINUE");
+
+
+        String[] names = {"tinyiko", "senior", "chauke", "velaphi", "shadrack"};
+        String[] lastNames = {"ngomani", "chabalala", "chauke", "smith", "vanberik"};
+
+        for (String name : names) {
+            if (name.contains("elaphi")) {
+                System.out.println(name + " contains elaphi");
+                break;
+            }
+            System.out.println(name);
+        }
+
+        for (String lastName : lastNames) {
+            if (lastName.startsWith("c")){
+                continue; // any lastName starting with "c" will be ignored
+            }
+            System.out.println(lastName); //any lastName starting with "c" wont be printed
+        }
+
     }
 }
