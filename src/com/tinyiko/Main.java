c16bcf1d420875138cda5b56619ae6f98c5664d5
f5295785 <tinyiko.chauke1@fnb.co.za>
2021-08-09 18:42:12 +0200

    try catch block

--- src/com/tinyiko/Main.java
+++ src/com/tinyiko/Main.java
@@ -4,5 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world this is my first java 101 program");
+
+        String a = null;
+        String b = "";
+
+        try {
+            b.substring(1);
+            a.substring(1);
+        } catch (NullPointerException e) {
+            System.out.println("null pointer "+e.getMessage());
+        }catch (StringIndexOutOfBoundsException e){
+            System.out.println("null pointer "+e.getMessage());
+        }catch (Exception e){
+            System.out.println("null pointer "+e.getMessage());
+        }
+
     }
 }
