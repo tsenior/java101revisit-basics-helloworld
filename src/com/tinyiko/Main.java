bc05549a5386096d5b5ceb249b3899e637b3ce33
f5295785 <tinyiko.chauke1@fnb.co.za>
2021-08-09 17:58:16 +0200

    passing by copy using primitive types

--- src/com/tinyiko/Main.java
+++ src/com/tinyiko/Main.java
@@ -4,5 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world this is my first java 101 program");
+
+        int originalValue = 10;
+
+        System.out.println("original value before :"+originalValue);
+        incrementValue(originalValue);
+        System.out.println("original value after:"+originalValue);
+
+
+
+    }
+    public static void incrementValue(int value){
+        value += 1;
+        System.out.println("value after incremented by 1 :"+value );
+
     }
 }
