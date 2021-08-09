2b4280816055a00708618decca24776e37d62d96
f5295785 <tinyiko.chauke1@fnb.co.za>
2021-08-09 17:01:30 +0200

    looping techniqs

--- src/com/tinyiko/Main.java
+++ src/com/tinyiko/Main.java
@@ -1,8 +1,43 @@
 package com.tinyiko;
 
+import com.sun.source.tree.WhileLoopTree;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world this is my first java 101 program");
+
+        String[] months = {"janaury", "february", "march",
+                           "april", "may", "june",
+                           "july", "august", "september",
+                           "october", "november", "december"};
+
+        System.out.println("***********for i loop***********");
+        for (int i = 0; i < months.length; i++) {
+            System.out.println(months[i]);
+        }
+
+        System.out.println("***********Dfor each loop***********");
+        for (var month : months){
+            System.out.println(month);
+        }
+
+
+        System.out.println("***********while loop***********");
+        int whileCounter = 0;
+
+        while (whileCounter < months.length){
+            System.out.println(months[whileCounter]);
+            whileCounter++;
+        }
+
+       System.out.println("***********Do while loop***********");
+       int doCounter = 0;
+
+       do{
+            System.out.println(months[doCounter]);
+            doCounter++;
+        } while (doCounter < months.length);
+
     }
 }
