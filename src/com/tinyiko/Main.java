d079472d6e5f12d2bb61714b3f3b670893453e11
f5295785 <tinyiko.chauke1@fnb.co.za>
2021-08-09 12:33:55 +0200

    Interpolate strings with placeholders

--- src/com/tinyiko/Main.java
+++ src/com/tinyiko/Main.java
@@ -4,5 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world this is my first java 101 program");
+
+        var item = "Shirt";
+        var size = "M";
+        var price = 14.99;
+        var color = "Red";
+
+        var stringTemplate = "Clothing item: %s, size %s, color %s, R%.2f";
+        var itemString = String.format(stringTemplate, item,
+                size, color, price);
+
+        System.out.println(itemString);
     }
 }
