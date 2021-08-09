f29b5a4f764b5efe49eba57f7e8c6e23e7c3246e
f5295785 <tinyiko.chauke1@fnb.co.za>
2021-08-09 20:17:20 +0200

    Close objects with try-with-resources

--- src/com/tinyiko/Main.java
+++ src/com/tinyiko/Main.java
@@ -1,8 +1,29 @@
 package com.tinyiko;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world this is my first java 101 program");
+
+        var file = new File("hello.txt");
+        if (!file.exists()) {
+            System.out.println("file does not exist");
+        }
+
+        try (FileReader fileReader = new FileReader(file);
+             BufferedReader bufferedReader = new BufferedReader(fileReader)){
+
+            var text = bufferedReader.readLine();
+            System.out.println(text);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
