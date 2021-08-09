8aadacdfb2a5b145c0c3794d56979fd555b45849
f5295785 <tinyiko.chauke1@fnb.co.za>
2021-08-09 17:19:17 +0200

    a claculater that uses methods for code reuse

--- src/com/tinyiko/Main.java
+++ src/com/tinyiko/Main.java
@@ -1,8 +1,22 @@
 package com.tinyiko;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world this is my first java 101 program");
+
+        var sc = new Scanner(System.in);
+
+        double d1 = getInput(sc, "Enter value 1: ");
+        double d2 = getInput(sc, "Enter value 2: ");
+
+        System.out.println(d1 + " divided by "+ d2 +" equals :"+(d1 / d2));
+    }
+
+    private static double getInput(Scanner sc, String prompt) {
+        System.out.println(prompt);
+        return sc.nextDouble();
     }
 }
