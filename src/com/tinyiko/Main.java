980a14d68adf9cb41dc8e0cad9dea5e1a5f9b6fd
f5295785 <tinyiko.chauke1@fnb.co.za>
2021-08-10 01:35:32 +0200

    Added a model class - data (class variables) - class methods
    implemented
    - instance variable
    - sets instance variable data
    - String Interpolate

--- src/com/tinyiko/Main.java
+++ src/com/tinyiko/Main.java
@@ -1,8 +1,24 @@
 package com.tinyiko;
 
+import com.tinyiko.model.ClothingItem;
+
+import java.text.NumberFormat;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world this is my first java 101 program");
+
+        var item = new ClothingItem();
+        item.setType("Shirt");
+        item.setSize("M");
+        item.setPrice(19.99);
+        item.setQuantity(3);
+
+        var totalCost = item.getPrice() * item.getQuantity();
+        var formatter = NumberFormat.getCurrencyInstance();
+        var slip = String.format("Your %s order will cost %s",
+                item.getType(), formatter.format(totalCost));
+
+        System.out.println(slip);
     }
 }
