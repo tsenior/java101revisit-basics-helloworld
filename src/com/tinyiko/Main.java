fc234a8114709e5182d50ca8a7d62ae12519c64f
f5295785 <tinyiko.chauke1@fnb.co.za>
2021-08-10 11:41:44 +0200

    replaced a normal array with a resizable array List for both simplest and complex objects

--- src/com/tinyiko/Main.java
+++ src/com/tinyiko/Main.java
@@ -6,32 +6,34 @@ import com.tinyiko.model.Hat;
 import com.tinyiko.model.Shirt;
 
 import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        String[] colors = new String[3];
-        colors[0] = "Red";
-        colors[1] = "Blue";
-        colors[2] = "Green";
 
+        List<String> colors = new ArrayList<>();
+        colors.add("Red");
+        colors.add("Blue");
+        colors.add("Green");
 
-        for (int i = 0; i < colors.length; i++) {
-            System.out.println(colors[i]);
+        for (int i = 0; i < colors.size(); i++) {
+            System.out.println(colors.get(i));
         }
 
-        ClothingItem[] items = {
+        List<ClothingItem> items = new ArrayList<>();
+        items.add(
                 new Shirt(
-                ClothingSize.M,
-                19.99,
-                3),
-
+                    ClothingSize.M,
+                    19.99,
+                    3));
+        items.add(
                 new Hat(
-                ClothingSize.M,
-                20.00,
-                1)
-        };
+                        ClothingSize.M,
+                        20.00,
+                        1));
 
       for (ClothingItem item : items){
           displayItemDetails(item);
